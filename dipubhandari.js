824ba872829d2f73524bcfe9b4b25539e768d246
dipubhandari <dipendrabhandari123456@gmail.com>
2022-12-23 11:06:16 +0545

    dipu bhandari

---
+++ dipubhandari.js
@@ -0,0 +1,13 @@
+console.log('Learning git hub')
+
+// diaplay the name from the object
+
+const students = [
+    { name: 'Dipu bhandari', age: 0 },
+    { name: 'Dipu bhandari', age: 0 },
+    { name:'Dipu bhandari',age:0},
+    {name:'Dipu bhandari',age:0},
+    { name: 'Dipu bhandari', age: 0 },
+]
+
+students.map((first,secibd))
